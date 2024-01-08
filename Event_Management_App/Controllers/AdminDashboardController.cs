c95d3843af24dd6d5a1c875762aa4d43c68ed30f
Pranay Kalamkar <pranaykalamkar08@gmail.com>
2024-01-08 18:43:12 +0530

    New Updates

--- Event_Management_App/Controllers/AdminDashboardController.cs
+++ Event_Management_App/Controllers/AdminDashboardController.cs
@@ -4,6 +4,21 @@ namespace Event_Management_App.Controllers
 {
     public class AdminDashboardController : Controller
     {
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly ILogger<AdminDashboardController> _logger;
+
+        public AdminDashboardController(ILogger<AdminDashboardController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
